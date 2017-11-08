3da37752f5e5a3250e25fe9b2d9640280090e686
Elliot Richardson <ear78@aol.com>
2017-11-08 00:52:39 -0600

    worked on creating edit functionality with routing in ember, need to debug

--- app/components/bug-editor/component.js
+++ app/components/bug-editor/component.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const BugEditorComponent = Ember.Component.extend({
   actions: {
     save: function(model){
       let component = this;
       model.save().then(()=>{
+        console.log('model saved');
         component.sendAction('handleRedirect');
       })
     }
   }
 });
+
+BugEditorComponent.reopenClass({
+  positionalParams: ['bug']
+});
+
+export default BugEditorComponent;
